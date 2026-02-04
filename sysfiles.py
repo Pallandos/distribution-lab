10f2648c7b689bfee9a816d815d2cafd2e5269d4
Pallandos <commit@pallandos.dev>
2026-02-04 15:45:54 +0100

    add exo1 (no tests)

--- sysfiles.py
+++ sysfiles.py
@@ -1,8 +1,13 @@
 import os
+from PIL import Image
+import io
 
 class FS():
-    def __init__(self, dir_t) -> None:
+    def __init__(self, dir_t, image_t, file_t) -> None:
         self.dir = dir_t
+        self.image = image_t
+        self.file = file_t
+        self.array = bytearray()
     
     def create_dir(self) -> None:
         os.mkdir(self.dir)
@@ -10,3 +15,29 @@ class FS():
     def read_dir(self) -> None:
         content = os.listdir(self.dir)
         print(f"Content of {self.dir} :\n {content}")
+        
+    def display(self):
+        f = os.open(self.image, os.O_RDONLY)
+        try:
+            while True:
+                b = os.read(f, 1)
+                if not b:
+                    break
+                self.array.append(b[0])
+        finally:
+            os.close(f)
+            self._display_array()
+    
+    def _display_array(self):
+        image = Image.open(io.BytesIO(self.array))
+        image.show()
+        
+    def write(self):
+        f = os.open(self.file, os.O_APPEND | os.O_CREAT | os.O_TRUNC)
+        try: 
+            os.write(f, self.array)
+        finally:
+            os.close(f)
+        
+if __name__ == "__main__":
+    pass
